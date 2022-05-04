e873e8ede4028d05e3170a26f790c2f43d623583
mazeina <>
2022-05-04 18:47:06 +0300

    добавлен GameViewController

--- Alias/Constants/Constant.swift
+++ Alias/Constants/Constant.swift
@@ -13,6 +13,9 @@ struct K {
         static let primaryInterfaceBackground: UIColor = .init(red: 10, green: 161, blue: 221)
         static let secondaryInterfaceBackground: UIColor = .init(red: 255, green: 140, blue: 50)
         static let cardBackground: UIColor = .init(red: 239, green: 239, blue: 239)
+        static let correctButtonColor: UIColor = .init(red: 91, green: 146, blue: 89)
+        static let skipButtonColor: UIColor = .init(red: 179, green: 48, blue: 48)
+        static let secondaryButtonColor: UIColor = .init(red: 21, green: 114, blue: 161)        
     }
     
     struct Numeric {
---
+++ Alias/Game/CircularProgressBarView.swift
@@ -0,0 +1,77 @@
+//
+//  CircularProgressBarView.swift
+//  Alias
+//
+//  Created by Полина Дусин on 04.05.2022.
+//
+
+import UIKit
+
+let gameVC = GameViewController()
+
+class CircularProgressBarView: UIView {
+    
+    private var progressLayer = CAShapeLayer()
+    private var circleLayer = CAShapeLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createCircularPath()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        createCircularPath()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        createCircularPath()
+    }
+    
+    private func createCircularPath() {
+        let endPoint = -CGFloat.pi / 2
+        let startPoint = 2 * CGFloat.pi + endPoint
+        
+        // created circularPath for circleLayer and progressLayer
+        let circularPath = UIBezierPath(
+            arcCenter: CGPoint(x: frame.size.width/2, y: frame.size.height/2),
+            radius: 20,
+            startAngle: startPoint,
+            endAngle: endPoint,
+            clockwise: false
+        )
+        
+        // circleLayer path defined to circularPath
+        circleLayer.path = circularPath.cgPath
+        // ui edits
+        circleLayer.fillColor = UIColor.clear.cgColor
+        circleLayer.lineCap = .round
+        circleLayer.lineWidth = 5.0
+        circleLayer.strokeEnd = 1.0
+        circleLayer.strokeColor = UIColor.white.cgColor
+        // added circleLayer to layer
+        layer.addSublayer(circleLayer)
+        
+        // progressLayer path defined to circularPath
+        progressLayer.path = circularPath.cgPath
+        // ui edits
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.lineCap = .round
+        progressLayer.lineWidth = 5.0
+        progressLayer.strokeEnd = 1.0
+        progressLayer.strokeColor = UIColor.green.cgColor
+        // added progressLayer to layer
+        layer.addSublayer(progressLayer)
+    }
+    
+    func circleAnimation() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        basicAnimation.toValue = 0
+        basicAnimation.duration = CFTimeInterval(gameVC.durationTimer)
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = true
+        progressLayer.add(basicAnimation, forKey: "progressAnimation")
+    }
+}
